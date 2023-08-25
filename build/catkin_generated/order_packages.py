2f85d3b41a9313e2994e4c16510fc8e6ed571f5c
Juan Camilo <camherrera837@hotmail.com>
2023-08-24 21:50:29 -0500

    subscriber_and_publisher

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/juan_c/manipuladorbot_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/juan_c/tutorial_ws/devel;/home/juan_c/drone_workspace/devel;/home/juan_c/catkin_ws/devel;/home/juan_c/my_workspace/devel;/opt/ros/melodic'.split(';') if '/home/juan_c/tutorial_ws/devel;/home/juan_c/drone_workspace/devel;/home/juan_c/catkin_ws/devel;/home/juan_c/my_workspace/devel;/opt/ros/melodic' != '' else []
---
+++ build/manipuladorbot_test/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/juan_c/manipuladorbot_ws/src/manipuladorbot_test
+# Build directory: /home/juan_c/manipuladorbot_ws/build/manipuladorbot_test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
