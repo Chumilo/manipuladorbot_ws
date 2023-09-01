bf4aa20c28173959ed717a946d2ac781f3b33c71
Juan Camilo <camherrera837@hotmail.com>
2023-08-31 20:04:35 -0500

    configuracion_pid

--- src/manipulador_description/scripts/mover_brazo.py
+++ src/manipulador_description/scripts/mover_brazo.py
@@ -9,20 +9,30 @@ def nodo_manipulador():
     
     publisher_1 = rospy.Publisher('/joint_1_controller/command',Float64,queue_size=1)
     publisher_2 = rospy.Publisher('/joint_2_controller/command',Float64,queue_size=1)
+    publisher_3 = rospy.Publisher('/joint_3_controller/command',Float64,queue_size=1)
+    publisher_4 = rospy.Publisher('/joint_4_controller/command',Float64,queue_size=1)
     
     msg_1 = Float64()
     msg_2 = Float64()
+    msg_3 = Float64()
+    msg_4 = Float64()
     
-    msg_1.data = 0.0
-    msg_2.data = 1.0
+    msg_1.data = 3.0    #rango 0-  3.14
+    msg_2.data = 1.5    #rango -1.57 - 1.57
+    msg_3.data = 1.0    #rango 0 - 3.14
+    msg_4.data = 1.0    #rango -3.14 - 3.14
     
     rate = rospy.Rate(10)  #crea un objeto rate a 10hz
     
     while not rospy.is_shutdown():
         
-        #publisher_1.publish(msg_1)
+        publisher_1.publish(msg_1)
         rospy.sleep(1)
         publisher_2.publish(msg_2)
+        rospy.sleep(1)
+        publisher_3.publish(msg_3)
+        rospy.sleep(1)
+        publisher_4.publish(msg_4)
 
 if __name__ == '__main__':
     try:
